675263d8b774437fdc3db0b05d3104bcb4dfb111
Jason Peavy <32001022+jason6297@users.noreply.github.com>
2019-10-16 18:06:31 -0500

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -1,14 +1,21 @@
-//tenny merged the parser and driver
-public class Parser {
-	Token current; //holds current
+//Jason Peavy & Hannah Saffel
+//CSC 415 Assignment 5
+//PIFL language lex & parse
+
+import java.io.File;
+
+public class Parser 
+{
+	Token current; //holds current token
 	Lexer lexer; //holds our lexer
 	
 	public Parser(String filename)
 	{
 		try
 		{
-			lexer = new Lexer(new File(filename));
+			lexer = new Lexer(filename);
 			current = lexer.lex();
+			program();
 		}
 		catch(Exception e)
 		{
@@ -19,86 +26,159 @@ public class Parser {
 	public void program() 
 	{
 		System.out.println("Begin <program>");
-		//try
-		//{
-		//	
-		//}
-		//catch(Exception e)
-		//{
-		//	System.out.println(currentToken + " does not match a valid statement");
-		//}
-		//finally
-		//{
-		//kill program
-		//}
+		stmts();
 	} 
-	public void match(String token)
+	public void match(Token token)
 	{
-		if(token.equals(next)) {
+		if(token.equals(current)) 
+		{
 			System.out.println("Matched " + token.getDescription());
-			//if(scanner.hasNext())
-			//	next = scanner.next();
-			//else
-			//	System.exit(0);
-			try {
-				lexer.getLexeme();
+			try
+			{
+				current = lexer.lex();
 			}
-			catch (Exception e) {
+			catch (Exception e) //end of file 
+			//TODO: make sure reached end of file successfully (not expecting more tokens)
+			{
 				System.out.println("End of file reached [error in match()]");
 				System.exit(0);
 			}
 		}
-		else {
-		System.out.println("ERROR"); //change this to be more descriptive
+		else 
+		{
+		System.out.println("ERROR"); 
+		//TODO: change this to be more descriptive
 		System.exit(0);
 		}
 	}
-	public void stmts()
-	{		
-		System.out.println("Begin <stmts>");
-		match(.);
+	public void stmts() //<stmts> -> <stmt> {<stmt>}
+	{	
+		try
+		{
+			System.out.println("Begin <stmts>");
+			//TODO: make sure this is actually valid lol
+			stmt();
+		}
+		catch (Exception e)
+		{
+			System.out.println("incorrext statements syntax: \"  " + current.getLexeme() + "\"");
+			e.printStackTrace();
+		}
 	} 
-	public void stmt()
+	public void stmt() //<stmt> -> <decl_stmt> | <ass_stmt> | <loop_stmt> | <if_stmt> 
 	{
-		System.out.println("Begin <stmt>");
-		//check if the following statement is a valid type
+		try {
+			System.out.println("Begin <stmt>");
+			switch(current.getDescription()) 
+			{
+				case "num keyword":
+					decl_stmt();
+					break;
+				case "string keyword":
+					decl_stmt();
+					break;
+				case "repeat keyword":
+					loop_stmt();
+					break;
+				case "if keyword":
+					if_stmt();
+					break;
+				case "show keyword":
+					print_stmt();
+					break;
+				case "identifier":
+					ass_stmt();
+					break;
+				default:
+					throw new Exception();
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println("incorrext statement syntax: \" " + current.getLexeme() + "\"");
+			e.printStackTrace();
+		}
 	} 
-	public void decl_stmt()
+	
+	public void decl_stmt() //<decl_stmt> -> (num|string) %identifier%
 	{
 		System.out.println("Begin <decl_stmt>");
-		match("");
+		match(new Token(current.getLexeme(), "<decl_stmt>"));
 	} 
-	public void ass_stmt()
+	
+	public void ass_stmt() //<ass_stmt> -> %identifier% = <expr>
 	{
-		
+		System.out.println("Begin <ass_stmt>");
+		match(new Token(current.getLexeme(), "identifier"));
 	} 
-	public void loop_stmt()
+	
+	public void loop_stmt() throws Exception //<loop_stmt> -> repeat (<expr> | <bool_expr>) \( <stmts> \)
 	{
-		
+		System.out.println("Begin <loop_stmt>");
+		match(new Token(current.getLexeme(), "repeat keyword"));
+		expr();
+		match(new Token(current.getLexeme(), "open parenthesis"));
+		stmts();
+		match(new Token(current.getLexeme(), "close parenthesis"));
 	} 
-	public void if_stmt()
+	
+	public void if_stmt() //<if_stmt> -> if <bool_expr> \( <stmts> \)
 	{
-		
+		System.out.println("Begin <if_stmt>");
+		bool_expr();
+		match(new Token(current.getLexeme(), "open parenthesis"));
+		stmts();
+		match(new Token(current.getLexeme(), "close parenthesis"));
 	} 
-	public void print_stmt()
+	
+	public void print_stmt() //<print_stmt> -> show \( <expr> \)
 	{
-		
+		System.out.println("Begin <print_stmt>");
+		match(new Token(current.getLexeme(), "show keyword"));
+		match(new Token(current.getLexeme(), "open parenthesis"));
+		expr();
+		match(new Token(current.getLexeme(), "close parenthesis"));
 	} 
-	public void expr()
+	
+	public void expr() //<expr> -> <val> { + <val>}
 	{
-		
+		try {
+			System.out.println("Begin <expr>");
+			switch(current.getDescription()) 
+			{
+				case "string literal":
+					match(new Token(current.getLexeme(), "string literal"));
+					break;
+				case "numeric literal":
+					match(new Token(current.getLexeme(), "numeric literal"));
+					break;
+				default:
+					throw new Exception();
+			}
+		}
+		catch (Exception e) {
+			System.out.println("incorrext expression syntax: \" " + current.getLexeme() + "\"");
+			e.printStackTrace();
+		}
 	} 
-	public void bool_expr()
+	
+	public void bool_expr() //<bool_expr> -> <expr> : <expr>
 	{
-		System.out.println("Begin <bool_expr>");		
+		System.out.println("Begin <bool_expr>");
+		expr();
+		match(new Token(current.getLexeme(), "is equal to"));
+		expr();
 	} 
-	public void val()
+	
+	public void val() //<val> -> %identifier% | %numeric_literal% | %string_literal%
 	{
-		
+		System.out.println("Begin <val>");
+		match(new Token(current.getLexeme(), "<val>"));
 	} 
-	public static void main(String[] args) {
-		Parser parser = new Parser(args[0]);
-		parser.program();
-		//we gonna have to make sure the lexer works right 
-	}
+//	public static void main(String[] args) 
+//	{
+//		Parser parser = new Parser(args[0]);
+//		parser.program();
+//		//TODO: make sure the lexer works right 
+//	}
 }
